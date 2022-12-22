aceff73e1facf4d047073eb378ef9b8a0af439cf
Marco Antonio GarcíaG <g2marco@yahoo.com.mx>
2022-12-21 22:53:51 -0600

    Actualizacion de archivos de configuración para ambiente windows.

--- pic_gui/dist/consulta_rango.bat
+++ pic_gui/dist/consulta_rango.bat
@@ -1,4 +1,4 @@
 @echo on
 
-for /L %%I in ( 60, 1, 90) DO ping -w 30 -n 1 192.168.1.%%I | find "Respuesta"
+for /L %%I in ( 20, 1, 130) DO ping -w 30 -n 1 192.168.1.%%I | find "Respuesta"
 pause
--- pic_gui/dist/programmer.bat
+++ pic_gui/dist/programmer.bat
@@ -1,3 +1,3 @@
 @echo off
-
-call java -classpath ./libs/*;pic.programmer.gui-1.0.jar mx.com.neogen.pic.programmer.gui.Main
+set JAVA_HOME=D:\software\java\jdk-18.0.1.1
+call %JAVA_HOME%\bin\java -classpath ./libs/*;pic.programmer.gui-1.0.jar mx.com.neogen.pic.programmer.gui.Main
