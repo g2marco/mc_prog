0aa91697297b53f52f0c1c8e503c650e91322a1c
g2marco <mgarcia@eurekaconsulting.mx>
2023-02-17 23:00:00 -0600

    Reestructuración de codigo C   - Reestructuracion de módulo adaptador_pp

--- pp_driver/adaptador_pp/test/compilar.sh
+++ pp_driver/adaptador_pp/test/compilar.sh
@@ -4,7 +4,7 @@
 echo "Compiling Adaptador PP Test"
 
 rm -f 01_pp_adapter.test
-gcc 01_pp_adapter.c  ../adaptador_pp.c ../puerto_paralelo/puerto_paralelo.c -o 01_pp_adapter.test
+gcc 01_pp_adapter.c  ../adaptador_pp.c ../../puerto_paralelo/puerto_paralelo.c -o 01_pp_adapter.test
 
 sudo chown root.root 01_pp_adapter.test
 sudo chmod +s        01_pp_adapter.test
