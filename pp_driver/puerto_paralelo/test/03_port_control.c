6e86318469e4e0687cfa22c111feacf0fbe7ac3c
Marco Antonio Garci­a G <mgarcia@eurekaconsulting.mx>
2023-11-26 00:01:30 -0600

    Update 03_port_control.c

--- pp_driver/puerto_paralelo/test/03_port_control.c
+++ pp_driver/puerto_paralelo/test/03_port_control.c
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <signal.h>
 
@@ -28,11 +29,18 @@
 FILE * log_file = NULL;                             // log file path
 
 
+unsigned short char_to_int( char n) {
+    return (unsigned short) ( n - '0');
+}
+
+
 //  updates whole data register with a value
 
 void update_data_port( unsigned int value) {
     port.data.value = value;
     
+    printf( "\nupdating data[:] = %i", value);
+
     write_data_port();
 }
 
@@ -49,8 +57,9 @@ void update_data_bit( unsigned int idx, unsigned int value) {
         case 5: port.data.bits.D5 = value; break;
         case 6: port.data.bits.D6 = value; break;
         case 7: port.data.bits.D7 = value; break;
-
     }
+
+    printf( "\nupdating data[%i] = %i\n", idx, value);
 
     write_data_port();
 }
@@ -66,6 +75,8 @@ void update_control_bit ( unsigned int idx, unsigned int value) {
         case 3: port.control.bits.C3 = value; break;
     }
 
+    printf( "\nupdating control[%i] = %i\n", idx, value);
+
     write_control_port();
 }
 
@@ -78,27 +89,72 @@ void read_and_print() {
     print_registros();
 }
 
-typedef struct {  
+typedef struct {
     unsigned short estatus;         // 0 tarea valida, 1 tarea invalida
     unsigned short opcion;          // 0 write data, 1 write data bit, 2 write control bit, 3 print status
     unsigned short idx;             // data: 0 - 7, control 0 - 3
     unsigned short value;           // register 0 -55, bit 0 : 1
 } tarea;
 
+
 tarea read_params( int argc, char* argv[]) {
     tarea t;
 
     t.estatus = 0;
 
-    swtich ( argc) {
+    switch ( argc) {
         case 1:     // no params
             t.opcion = 3;
             break;
 
         case 3:     // data 0xAA | D1 0 | C3 0
-            t.opcion = 1;
-            t.value  = 255;
+            switch (argv[1][0]) {
+                case 'd':
+                    t.opcion = 0;
+
+                    long value = strtol( argv[2], NULL, 0);
+
+                    if ( value < 0 || value > 255) {
+                        // error > valor fuera de rango
+                        t.estatus = 1;
+                    } else {
+                        t.value  = (unsigned short) value;
+                    }
+
+                    break;
+
+                case 'D':
+                    t.opcion = 1;
+
+                    t.idx    = char_to_int( argv[1][1]);
+                    t.value  = char_to_int( argv[2][0]);
+
+                    if ( t.idx < 0 || t.idx > 7 || t.value < 0 || t.value > 1) {
+                        // error > valores no permitidos
+                        t.estatus = 1;
+                    }
+
+                    break;
+
+                case 'C':
+                    t.opcion = 2;
+
+                    t.idx    = char_to_int( argv[1][1]);
+                    t.value  = char_to_int( argv[2][0]);
+
+                    if ( t.idx < 0 || t.idx > 3 || t.value < 0 || t.value > 1) {
+                        // error > valores no permitidos
+                        t.estatus = 1;
+                    }
+
+                    break;
+
+                default:
+                    t.estatus = 1;
+            }
+
             break;
+
 
         default:    // invocation not allowed
             t.estatus = 1;
@@ -119,7 +175,7 @@ int main( int argc, char* argv[]) {
     tarea t = read_params( argc, argv);
 
     if ( t.estatus != 0) {
-        printf( "\n\tWrong invocation / parameters, please correct");
+        printf( "\n\tWrong invocation / parameters, please correct\n");
         return 0;
     }
 
@@ -139,8 +195,9 @@ int main( int argc, char* argv[]) {
         case 0: update_data_port(          t.value); break;
         case 1: update_data_bit(    t.idx, t.value); break;
         case 2: update_control_bit( t.idx, t.value); break;
-        case 3: read_and_print();                    break;
     }
+
+    read_and_print();
     
     release_puerto_paralelo();
     
