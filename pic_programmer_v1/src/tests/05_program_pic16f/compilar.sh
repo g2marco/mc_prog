3c17a0b88ae1626dc35fc8fdcfc0229c26ef87a8
Marco Antonio Garci­a G <mgarcia@eurekaconsulting.mx>
2023-12-08 10:03:07 -0600

    Actualización de pruebas de modulo de programación de PIC

--- pic_programmer_v1/src/tests/05_program_pic16f/compilar.sh
+++ pic_programmer_v1/src/tests/05_program_pic16f/compilar.sh
@@ -2,14 +2,17 @@
 #
 # This script compiles and execute test programs
 #
-echo "Compiling PIC16FXXX Programmer Test"
-
-rm -f 01_program_pic16f.x
 
 MODULES_PATH=../../modules
 INCLUDE_LIBS=-I../../libs
 
-gcc program_pic16f_test.c $MODULES_PATH/program_pic16f.c $MODULES_PATH/driver_pic16f.c $MODULES_PATH/program_info.c $MODULES_PATH/port_adapter.c  $MODULES_PATH/parallel_port.c -o 01_program_pic16f.x $INCLUDE_LIBS 
+
+echo "Compiling PIC16FXXX Programmer Test"
+
+rm -f 01_program_pic16f.x
+
+
+gcc program_pic16f_test.c $MODULES_PATH/program_pic16f.c $MODULES_PATH/program_info.c $MODULES_PATH/driver_pic16f.c $MODULES_PATH/port_adapter.c $MODULES_PATH/parallel_port.c -o 01_program_pic16f.x $INCLUDE_LIBS 
 
 sudo chown root.root 01_program_pic16f.x
 sudo chmod +s        01_program_pic16f.x
