f708142d6c60f227562cc57130f0c6415e665351
Marco Antonio Garci­a G <mgarcia@eurekaconsulting.mx>
2023-12-09 17:46:37 -0600

    actualización de prueba para pic 12f683

--- pic_programmer_v1/src/modules/driver_pic16f.c
+++ pic_programmer_v1/src/modules/driver_pic16f.c
@@ -31,13 +31,13 @@
 //
 #define VCC_SETUP_TIME        10000                 //   10 ms
 #define VPP_SETUP_TIME        10000                 //   10 ms
-#define CLK_HOLD_TIME           500                 // 0.4 ms
+#define CLK_HOLD_TIME           500                 // 0.5 ms
 
 
-#define COMMAND_EXECUTION_TIME                10000      // tiempo para ejecución de comando
-#define COMMAND_SETUP_TIME                    10000      // tiempo antes de envio de dato
+#define COMMAND_EXECUTION_TIME                 1000      // tiempo para ejecución de comando
+#define COMMAND_SETUP_TIME                     1000      // tiempo antes de envio de dato
 #define COMMAND_PROGRAMMING_EXECUTION_TIME    10000
-#define INTER_COMMAND_TIME                    10000
+#define INTER_COMMAND_TIME                     1000
     
 //
 //  Métodos de manipulación de señales de control
