86943f3eb1d9ab0edc919212f36530adf6320567
mgarcia@eurekaconsulting.mx <g2marco@2806-107e-001f-0907-07ba-5ae7-c537-ce7a.ipv6.infinitum.net.mx>
2024-06-26 07:07:01 -0600

    inicio de espacio de trabajo en linux

--- pic_gui/dist/startup.sh
+++ pic_gui/dist/startup.sh
@@ -6,5 +6,5 @@
 # exit when any error
 set -e
 
-export JAVA_HOME=/home/g2marco/software/java/jdk-18.0.2.1
+export JAVA_HOME=/home/g2marco/software/java/jdk-18.0.2
 $JAVA_HOME/bin/java -cp "./libs/pic.programmer.gui-1.0.jar" mx.com.neogen.pic.programmer.gui.Main
