600d0dae5409b279bf7190dc937fb544e4abd0d4
Marco Antonio García G. <g2marco@yahoo.com.mx>
2024-09-30 22:37:32 -0600

    actualización de carpetas

--- workspace/workspace.sh
+++ workspace/workspace.sh
@@ -2,4 +2,4 @@
 #  ejeución de netbeans IDE
 
 clear
-netbeans --nosplash --jdkhome /home/g2marco/software/java/jdk-18.0.2 --open-group c_code_mc &
+netbeans --nosplash --jdkhome /home/g2marco/software/java/jdk-21.0.2 --open-group c_code_mc &
