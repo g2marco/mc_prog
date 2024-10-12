91c06df7e8e25ea27d1b99273bb0b8dfa558aa26
Marco Antonio García G. <g2marco@yahoo.com.mx>
2024-10-12 17:43:24 -0600

    Prueba de operaciones de borrado

--- pic_programmer_v1/src/modules/erase_device.c
+++ pic_programmer_v1/src/modules/erase_device.c
@@ -48,7 +48,7 @@ static void bulk_erase_type_2( unsigned char memoryArea) {
         execute_command( BULK_ERASE_MEM_PROGRAMA);
 
         // begin programming
-        execute_command( INICIA_CICLO_PROGRAM);
+        execute_command( INICIA_CICLO_ERASE_PROGRAM);
 
         wait_for( 10000);
     }
@@ -63,7 +63,7 @@ static void bulk_erase_type_2( unsigned char memoryArea) {
         execute_command( BULK_ERASE_MEM_DATOS );
 
         // begin programming
-        execute_command( INICIA_CICLO_PROGRAM);
+        execute_command( INICIA_CICLO_ERASE_PROGRAM);
         
         wait_for( 10000);
     }
@@ -104,7 +104,6 @@ static void disable_code_protection_type_2( unsigned short dato) {
     execute_command( 0x01, COMANDO_SIMPLE, 0);
     execute_command( 0x07, COMANDO_SIMPLE, 0);
 
-    wait_for( 10000);
 }
 
 void disable_code_protection( EraseOpts * eraseOpts) {
