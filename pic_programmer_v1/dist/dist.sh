7d8a4eb062f157bb60ef39d73d00ec5c5f925cb0
Marco Antonio García G. <g2marco@yahoo.com.mx>
2024-10-12 00:35:19 -0600

    Prueba de operaciones de borrado

--- pic_programmer_v1/dist/dist.sh
+++ pic_programmer_v1/dist/dist.sh
@@ -11,7 +11,7 @@ TARGET_PATH=/home/g2marco/dist/pic_programmer
 
 rm -f $TARGET_PATH
 
-gcc $MAIN_PATH/pic16fxxx_programmer.c  $MODULES_PATH/program_pic16f.c $MODULES_PATH/program_info.c $MODULES_PATH/driver_pic16f.c $MODULES_PATH/port_adapter.c $MODULES_PATH/parallel_port.c -o $TARGET_PATH $INCLUDE_LIBS 
+gcc $MAIN_PATH/pic16fxxx_programmer.c $MODULES_PATH/erase_device.c $MODULES_PATH/program_pic16f.c $MODULES_PATH/program_info.c $MODULES_PATH/driver_pic16f.c $MODULES_PATH/port_adapter.c $MODULES_PATH/parallel_port.c -o $TARGET_PATH $INCLUDE_LIBS 
 
 sudo chown root.root $TARGET_PATH
 sudo chmod +s        $TARGET_PATH
